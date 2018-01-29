5522ab2c9d80ec9f7786efcfb9228837d1bb659f
Emily Drevets <drevets@gmail.com>
2018-01-29 15:55:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments){
+  var sentences = []; 
+  var (i = 0; i < musicians.length; i++) {
+    var sentence = musicians[i] + ' plays ' + instruments[i] + '.'
+    sentences.push(sentence)
+  }
+  return sentences
+}
+
+function johnLennonFacts(facts) {
+  var index = 0; 
+  while (index < facts.length) {
+    facts[index] = facts[index] + '!'
+    index +=1
+  }
+  return facts 
+}
+
+function iLoveTheBeatles(number) {
+  var beatleLove = []; 
+  var i = 0; 
+  do {
+    beatleLove.push('I love the Beatles!')
+    i++
+  }
+  while (i < number); 
+  return beatleLove
+}
+
